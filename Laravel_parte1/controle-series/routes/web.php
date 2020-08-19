cc8cf1de136c3af9d58c3bfb4b1e4e2c01eb76d9
DeiseClaudino <deise.claudino98@gmail.com>
2020-08-18 22:08:31 -0300

    Aprendendo a lidar com o Request

--- Laravel_parte1/controle-series/routes/web.php
+++ Laravel_parte1/controle-series/routes/web.php
@@ -12,4 +12,4 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/series', 'SeriesController@listarSeries');
+Route::get('/series', 'SeriesController@index');
