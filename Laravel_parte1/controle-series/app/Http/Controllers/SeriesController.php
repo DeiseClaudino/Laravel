e2b2f8e4118ab3e205ae428d7460e9ca0f4b78bd
DeiseClaudino <deise.claudino98@gmail.com>
2020-09-09 22:12:33 -0300

    Session e flash messages

--- Laravel_parte1/controle-series/app/Http/Controllers/SeriesController.php
+++ Laravel_parte1/controle-series/app/Http/Controllers/SeriesController.php
@@ -7,11 +7,12 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $series = Serie::query()->orderBy('nome')->get();
+        $mensagem = $request->session()->get('mensagem');
 
-        return view('series.index', compact('series'));
+        return view('series.index', compact('series', 'mensagem'));
     }
 
     public function create()
@@ -21,10 +22,8 @@ class SeriesController extends Controller
 
     public function store(Request $request)
     {
-        $nome = $request->nome;
-        $serie = Serie::create([
-            'nome'  =>  $nome
-        ]);
+        $serie = Serie::create($request->all());
+        $request->session()->flash('mensagem', "Série {$serie->id} criada com sucesso {$serie->nome}");
 
         return redirect('/series');
     }
--- Laravel_parte1/controle-series/resources/views/series/index.blade.php
+++ Laravel_parte1/controle-series/resources/views/series/index.blade.php
@@ -5,6 +5,11 @@ Séries
 @endsection
 
 @section('conteudo')
+@if(!empty($mensagem))
+<div class="alert alert-success">
+    {{ $mensagem }}
+</div>
+@endif
 
 <a href="/series/criar" class="btn btn-dark mb-2">Adicionar</a>
 
