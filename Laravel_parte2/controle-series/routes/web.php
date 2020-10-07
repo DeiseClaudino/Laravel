43dd3eb888c43516c617d518b858f779234b4949
DeiseClaudino <deise.claudino98@gmail.com>
2020-10-07 20:26:19 -0300

    Laravel parte 2: Preparando o ambiente

---
+++ Laravel_parte2/controle-series/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\SeriesController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/series', 'SeriesController@index')->name('listar_series');
+Route::get('/series/criar', 'SeriesController@create')->name('form_criar_series');
+Route::post('/series/criar', 'SeriesController@store');
+Route::delete('/series/{id}', 'SeriesController@destroy');
+
