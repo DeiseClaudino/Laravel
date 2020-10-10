c7240aec49a8873fed78cc2f763c6ce7f627813b
DeiseClaudino <deise.claudino98@gmail.com>
2020-10-09 22:51:02 -0300

    Exibindo as temporadas de uma série

--- Laravel_parte2/controle-series/resources/views/series/index.blade.php
+++ Laravel_parte2/controle-series/resources/views/series/index.blade.php
@@ -13,13 +13,18 @@ Séries
    @foreach($series as $serie)
    <li class="list-group-item d-flex justify-content-between align-items-center">
       {{ $serie->nome }}
-      <form method="post" action="/series/{{ $serie->id }}" onsubmit="return confirm('Tem certeza que deseja remover {{ addSlashes($serie->nome) }}?')">
-         @csrf
-         @method('DELETE')
-         <button class="btn btn-danger">
-            <i class="far fa-trash-alt"></i>
-         </button>
-      </form>
+      <span class="d-flex">
+        <a href="/series/{{ $serie->id }}/temporadas" class="btn btn-info btn-sm mr-1">
+            <i class="fas fa-external-link-alt"></i>
+        </a>
+        <form method="post" action="/series/{{ $serie->id }}" onsubmit="return confirm('Tem certeza que deseja remover {{ addSlashes($serie->nome) }}?')">
+            @csrf
+            @method('DELETE')
+            <button class="btn btn-danger">
+                <i class="far fa-trash-alt"></i>
+            </button>
+        </form>
+      </span>
    </li>
    @endforeach
 </ul>
--- Laravel_parte2/controle-series/routes/web.php
+++ Laravel_parte2/controle-series/routes/web.php
@@ -17,4 +17,5 @@ Route::get('/series', 'SeriesController@index')->name('listar_series');
 Route::get('/series/criar', 'SeriesController@create')->name('form_criar_series');
 Route::post('/series/criar', 'SeriesController@store');
 Route::delete('/series/{id}', 'SeriesController@destroy');
+Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
 
