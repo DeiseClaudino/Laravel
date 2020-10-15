0de3ab0ebfd3d9654a2e2cfd218e7d34ecbcfd5d
DeiseClaudino <deise.claudino98@gmail.com>
2020-10-14 21:45:54 -0300

    Resposta Desafio

--- Laravel_parte2/controle-series/app/Services/CriadorDeSerie.php
+++ Laravel_parte2/controle-series/app/Services/CriadorDeSerie.php
@@ -2,33 +2,48 @@
 
 namespace App\Services;
 use App\Serie;
+use App\Temporada;
+use Illuminate\Support\Facades\DB;
 
 class CriadorDeSerie
 {
     public function criarSerie(string $nomeSerie, int $qtdTemporada, int $epPorTemporada): Serie
     {
+        $serie = null;
+
+        DB::beginTransaction();
+
         $serie = Serie::create([
             'nome'  =>  $nomeSerie
         ]);
 
-        $qtdTemporadas = $qtdTemporada;
+        $this->criaTemporadas($qtdTemporada, $epPorTemporada, $serie);
 
+        DB::commit();
+
+        return $serie;
+
+    }
+
+    public function criaTemporadas(int $qtdTemporadas, int $epPorTemporada, Serie $serie)
+    {
         for($i = 1; $i <= $qtdTemporadas; $i++)
         {
             $temporada = $serie->temporadas()->create([
                 'numero'    =>  $i
             ]);
 
+            $this->criaEpisodios($epPorTemporada, $temporada);
         }
+    }
 
+    public function criaEpisodios(int $epPorTemporada, Temporada $temporada)
+    {
         for ($j = 1; $j <= $epPorTemporada; $j++)
         {
             $temporada->episodios()->create([
                 'numero' => $j
             ]);
         }
-
-        return $serie;
-
     }
 }
