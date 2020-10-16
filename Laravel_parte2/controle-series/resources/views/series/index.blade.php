62648d4dd6be58fbcecaa864958021fb99915800
DeiseClaudino <deise.claudino98@gmail.com>
2020-10-15 22:18:20 -0300

    Alterando o nome da série

--- Laravel_parte2/controle-series/resources/views/series/index.blade.php
+++ Laravel_parte2/controle-series/resources/views/series/index.blade.php
@@ -12,20 +12,46 @@ Séries
 <ul class="list-group">
    @foreach($series as $serie)
    <li class="list-group-item d-flex justify-content-between align-items-center">
-      {{ $serie->nome }}
+      <span id="nome-serie-{{ $serie->id }}">{{ $serie->nome }}</span>
+      <div class="input-group w-50" hidden id="input-nome-serie-{{ $serie->id }}">
+         <input type="text" class="form-control" value="{{ $serie->nome }}">
+         <div class="input-group-append">
+            <button class="btn btn-primary" onclick="editarSerie({{ $serie->id }})">
+            <i class="fas fa-check"></i>
+            </button>
+            @csrf
+         </div>
+      </div>
       <span class="d-flex">
-        <a href="/series/{{ $serie->id }}/temporadas" class="btn btn-info btn-sm mr-1">
-            <i class="fas fa-external-link-alt"></i>
-        </a>
-        <form method="post" action="/series/{{ $serie->id }}" onsubmit="return confirm('Tem certeza que deseja remover {{ addSlashes($serie->nome) }}?')">
+         <button class="btn btn-info btn-sm mr-1" onclick="toggleInput({{ $serie->id }})">
+         <i class="fas fa-edit"></i>
+         </button>
+         <a href="/series/{{ $serie->id }}/temporadas" class="btn btn-info btn-sm mr-1">
+         <i class="fas fa-external-link-alt"></i>
+         </a>
+         <form method="post" action="/series/{{ $serie->id }}"
+            onsubmit="return confirm('Tem certeza que deseja remover {{ addslashes($serie->nome) }}?')">
             @csrf
             @method('DELETE')
-            <button class="btn btn-danger">
-                <i class="far fa-trash-alt"></i>
+            <button class="btn btn-danger btn-sm">
+            <i class="far fa-trash-alt"></i>
             </button>
-        </form>
+         </form>
       </span>
    </li>
    @endforeach
 </ul>
 @endsection
+<script>
+   function toggleInput(serieId) {
+       const nomeSerieEl = document.getElementById(`nome-serie-${serieId}`);
+       const inputSerieEl = document.getElementById(`input-nome-serie-${serieId}`);
+       if (nomeSerieEl.hasAttribute('hidden')) {
+           nomeSerieEl.removeAttribute('hidden');
+           inputSerieEl.hidden = true;
+       } else {
+           inputSerieEl.removeAttribute('hidden');
+           nomeSerieEl.hidden = true;
+       }
+   }
+</script>
