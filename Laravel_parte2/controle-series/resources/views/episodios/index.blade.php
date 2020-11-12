db9acd0cfe4f07c0b18a6fc9fc7af7e74f84cc54
DeiseClaudino <deise.claudino98@gmail.com>
2020-11-11 22:24:10 -0300

    Listando os episódios

--- Laravel_parte2/controle-series/resources/views/episodios/index.blade.php
+++ Laravel_parte2/controle-series/resources/views/episodios/index.blade.php
@@ -1,18 +1,19 @@
 @extends('layout')
 
 @section('cabecalho')
-    Episódios
+Episódios
 @endsection
 
 @section('conteudo')
-
-    <ul class="list-group">
-        @foreach ($episodios as $episodio)
-        <li class="list-group-item d-flex justify-content-between align-itens-center">
-            Episódio {{ $episodio->numero }}
-        </li>
-        @endforeach
-    </ul>
-
-
+<form action="">
+   <ul class="list-group">
+      @foreach ($episodios as $episodio)
+      <li class="list-group-item d-flex justify-content-between align-itens-center">
+         Episódio {{ $episodio->numero }}
+         <input type="checkbox">
+      </li>
+      @endforeach
+   </ul>
+   <button class="btn btn-primary mt-2 mb-2">Salvar</button>
+</form>
 @endsection
