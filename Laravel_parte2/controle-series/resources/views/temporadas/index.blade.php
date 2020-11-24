3d891953225b3fd1956d21a0ade3af1acb5cdfaf
DeiseClaudino <deise.claudino98@gmail.com>
2020-11-23 21:46:26 -0300

    Assistindo episódios

--- Laravel_parte2/controle-series/resources/views/temporadas/index.blade.php
+++ Laravel_parte2/controle-series/resources/views/temporadas/index.blade.php
@@ -13,7 +13,7 @@
                 Temporada {{ $temporada->numero }}
             </a>
             <span class="badge badge-secondary">
-                0 / {{ $temporada->episodios->count() }}
+                {{ $temporada->getEpisodiosAssistidos()->count() }} / {{ $temporada->episodios->count() }}
             </span>
         </li>
         @endforeach
