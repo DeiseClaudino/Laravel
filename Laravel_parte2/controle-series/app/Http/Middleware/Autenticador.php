a1e9b9163f1a451c97cd3f8300a566c45c9a6a74
DeiseClaudino <deise.claudino98@gmail.com>
2020-12-11 21:10:48 -0300

    Middleware

--- Laravel_parte2/controle-series/app/Http/Middleware/Autenticador.php
+++ Laravel_parte2/controle-series/app/Http/Middleware/Autenticador.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
+use Illuminate\Support\Facades\Auth;
 
-class Authenticate extends Middleware
+class Autenticador
 {
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
@@ -14,8 +16,9 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return '/entrar';
+        if (!Auth::check()) {
+            return redirect('/entrar');
         }
     }
+
 }
