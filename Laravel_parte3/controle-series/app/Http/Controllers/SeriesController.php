93ffb832a36fb6a5da749a83c5fdc69d24519562
DeiseClaudino <deise.claudino98@gmail.com>
2021-01-13 22:18:48 -0300

    Implementando o envio

--- Laravel_parte3/controle-series/app/Http/Controllers/SeriesController.php
+++ Laravel_parte3/controle-series/app/Http/Controllers/SeriesController.php
@@ -46,8 +46,7 @@ class SeriesController extends Controller
 
             $email->subject = 'Nova Série Adicionada';
 
-            \Illuminate\Support\Facades\Mail::to($user)->send($email);
-            sleep(5);
+            \Illuminate\Support\Facades\Mail::to($user)->queue($email);
 
         }
 
